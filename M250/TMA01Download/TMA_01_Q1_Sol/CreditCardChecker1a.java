e70a2aab1f43cb0d8ecfdf9beba260d818c14e45
Tom Drew <ihavenolife@hotmail.co.uk>
2020-11-28 18:48:47 +0000

    new dir structure

---
+++ M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker1a.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class CreditCardChecker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CreditCardChecker
+{
+
+   private int ;
+
+   public CreditCardChecker()
+   {
+   {
+
+   }
+}
