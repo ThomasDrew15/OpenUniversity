d8c578322bfbc70c9e5a59db8944f869e29191a5
Tom Drew <ihavenolife@hotmail.co.uk>
2020-11-29 14:14:56 +0000

    isCorrectLength v1

--- M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
+++ M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
@@ -5,6 +5,10 @@
  * @author (Tom Drew)
  * @version (29/11/2020)
  */
+
+ /**
+  * Constructor for objects of class CreditCardChecker
+  */
  public class CreditCardChecker {
    private String longNumber;
 
@@ -17,4 +21,19 @@
      return longNumber;
 
    }
- }
+
+public boolean isCorrectLength(){
+  int length = longNumber.length();
+  if (length == 16){
+    System.out.println("success");
+  }
+}
+
+
+
+/**
+*no arguments and returns the boolean
+*value true if the length of longNumber
+*is 16 and false();
+*/
+}
