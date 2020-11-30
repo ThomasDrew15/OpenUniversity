2a07584f4240963ab1c615bc9d6d5e5d29a60ebe
Tom Drew <ihavenolife@hotmail.co.uk>
2020-11-30 01:16:58 +0000

    firstFifteen v1

--- M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
+++ M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
@@ -10,18 +10,32 @@
   * Constructor for objects of class CreditCardChecker
   */
  public class CreditCardChecker {
+   /**
+    * instance variable longNumber is
+    * a string
+    */
    private String longNumber;
 
+
+   /**
+    * Constructor for CCC
+    */
    public CreditCardChecker(String ln){
      longNumber = ln;
 
    }
-
+   /**
+    * getter method for longNumber
+    */
    public String getLongNumber(){
      return longNumber;
 
    }
-
+   /**
+    * boolean method to check longNumber
+    * is 16 digits long. True if so,
+    * false if not
+    */
    public boolean isCorrectLength(){
      if (longNumber.length() == 16){
        return true;
@@ -29,4 +43,10 @@
        return false;
      }
    }
+
+   public String firstFifteen(){
+     return longNumber.substring(int 1, int 15){
+   }
+
+
  }
