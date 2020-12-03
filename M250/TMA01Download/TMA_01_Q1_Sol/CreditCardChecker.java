33a69215c11e04ce4c11f2bf372f3e26e0486287
Tom Drew <ihavenolife@hotmail.co.uk>
2020-12-03 21:00:06 +0000

    v19

--- M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
+++ M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
@@ -50,7 +50,7 @@
 
    public void calculateCheckNumber(){
      String S = longNumber.substring(0, 15);
-     for (int i = 0, n = S.length(); i < n; i++) {
+     for (int i = 0, n = S.length(); i < n; i+2) {
        char c = S.charAt(i);
      System.out.println(S);
      System.out.print(c);
