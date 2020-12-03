8055a5cb751823f4331e905867b2d4cfb8c79b51
Tom Drew <ihavenolife@hotmail.co.uk>
2020-12-03 21:04:49 +0000

    testing for *2

--- M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
+++ M250/TMA01Download/TMA_01_Q1_Sol/CreditCardChecker.java
@@ -51,6 +51,7 @@
    public void calculateCheckNumber(){
      String S = longNumber.substring(0, 15);
      for (int i = 0, n = S.length(); i < n; i=i+2) {
+       int i = i*2;
        char c = S.charAt(i);
      //System.out.println(S);
      System.out.print(c);
