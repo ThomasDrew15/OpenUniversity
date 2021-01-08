b335c162fff3ddbd2a596ed4862d532e1c74cf21
Tom Drew <ihavenolife@hotmail.co.uk>
2021-01-08 10:15:31 +0000

    changes to dir structure

---
+++ M250/TMA_01/Unused_Files/CreditCardChecker1b.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class CreditCardChecker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+ public class CreditCardChecker {
+   private String longNumber;
+
+   public CreditCardChecker(String ln){
+     longNumber = ln;
+
+   }
+
+   public String getLongNumber(){
+     return longNumber;
+
+   }
+ }
