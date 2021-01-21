cdab1af814d55129ce51658fe23792070ff53469
Tom Drew <ihavenolife@hotmail.co.uk>
2021-01-21 21:42:21 +0000

    amended given code to fit class and variable names

---
+++ M250/TMA_02/TMA02Download/TMA_Q2_Sol/Dish.java
@@ -0,0 +1,202 @@
+
+/**
+ *
+ * Objects of the Dish class model real-world Dishs that have attributes
+ * representing style, year painted, insured value and whether or not an alarm 
+ * is required.
+ *
+ * Dish is an example of a composite class.
+ *
+ * An object of the Dish class has-an object of the Chef class,
+ * which represents the Chef who painted that Dish.
+ *
+ * The value of alarmRequired is computed and true is returned 
+ * if rating is equal to or greater than 1000. This is an example 
+ * of state-dependent behaviour.
+ * alarmRequired is not represented by an instance variable.
+ *
+ * @author Tom Drew
+ * @version 1.0 21/01/2021
+ */
+public class Dish
+{
+
+    //instance variables
+    private String style; // a Dish has-a style, e.g. "Italian"
+    private String dateLastMade; // a Dish has-a year when last made, e.g. "01062021"
+    private int rating; // the rating value, e.g. "2"
+    private Chef chef; // a Dish has-a chef of type Chef
+
+    /**
+     * Constructor for objects of class Dish
+     * allowing specification of all the required attributes.
+     */
+    public Dish(String aStyle, String aYear, int aValue, Chef theChef)
+    {
+        this.style = aStyle;
+        this.dateLastMade = aYear;
+        this.rating = aValue;
+        this.chef = theChef;
+    }
+
+    /**
+     * Constructor for objects of class Dish,
+     * where we only know the Dish style.
+     * This is the minimum information required to construct
+     * a Dish object. In this case we chose to make
+     * the Chef object 'empty'. 
+     */
+    public Dish(String astyle)
+    {
+        this.style = aStyle;
+        this.dateLastMade = null; //null indicates an unknown date
+        this.rating = 0; //defaults to 0 value
+        this.chef = new Chef();
+    }
+
+
+    //Setter methods are provided for all the instance variable
+    //so that these data can be altered in future if necessary,
+    //e.g. if the attribution of the Chef changes.
+
+//    /**
+//     * Setter for a Dish's Chef's name.
+//     * The name is an attribute of the component Chef object,
+//     * not the Dish per se. So we have to forward the message
+//     * to the component object.
+//     */
+//    public void setChefName(String aName)
+//    {
+//        this.chef.setName(aName);
+//    }
+
+    /**
+     * Setter for a Dish's Chef's name.
+     *This method first checks to see if there
+     *is an Chef component that can be sent a message. If there isn't
+     *one, it creates one.
+     */
+
+    public void setChefName(String aName)
+    {
+       //this message needs to be forwarded to the component Chef object
+       //but the Chef object might not exist yet, so we could check
+       if(this.Chef == null) //Has the Chef component been created?
+       {
+           //No, so we need to create a new Chef object and make
+           //the instance variable of the composite object to reference it
+           this.Chef = new Chef(aName);
+       }
+       else
+       {
+           //We already have an Chef object, so we just need to set
+           //its name by 'forwarding' the message to the Chef component
+           this.Chef.setName(aName);
+       }
+    }
+
+
+    /**
+     * Composition Supplement Exercise 3(b) 
+     * Add getter and setter methods for the Chef's yearOfQualification, 
+     * following the pattern set by setChefName() above.
+     */
+
+    /**
+     * Setter for a Dish's insured value.
+     */
+    public void setrating(int aValue)
+    {
+        this.rating = aValue;
+    }
+
+    /**
+     * Setter for a Dish's year painted.
+     */
+    public void setdateLastMade(String aYear)
+    {
+        this.dateLastMade = aYear;
+    }
+
+    /**
+     * Getter for a Dish's style.
+     */
+    public String getstyle()
+    {
+        return this.style;
+    }
+
+    /**
+     * Getter for a Dish's insured value.
+     */
+    public int getrating()
+    {
+        return this.rating;
+    }
+
+    /**
+     * Getter for a Dish's alarm requirement.
+     * This method displays state-dependent behaviour:
+     * The value returned depends on part of the state of the receiver.
+     * If the receiver's rating is greater than or equal to 1000 true is returned.
+     * Otherwise false is returned
+     */
+    public boolean isRecentlyUsed()
+    {
+        return this.getrating() >= 1000;
+    }
+
+    /**
+     * Getter for a Dish's year painted.
+     * This method returns "Unknown" if the year
+     * is not known (i.e. it has not been set).
+     */
+    public String getdateLastMade()
+    {
+        if (this.dateLastMade == null)
+        {
+            return "Unknown";
+        }
+
+        return this.dateLastMade;
+    }
+
+    /**
+     * Getter for a Dish's Chef's name.
+     * This method returns "Unattributed" if the Chef's name 
+     * is not known (i.e. it has not been set) and otherwise
+     * returns the Chef's name. Similar code could have
+     * been provided in the Chef class itself.
+     */
+    public String getChefName()
+    {
+        //The Chef's name is part of the component Chef object. 
+        //Does the component object exist?
+        if (this.Chef == null || this.Chef.getName() == null)
+        {
+            //no Chef or no Chef name was set up
+            return " Unattributed";
+        }
+
+        //The Chef component has been set up, 
+        //so we can forward this message to the component object 
+        return this.Chef.getName();
+    }
+
+    /**
+     * This method returns true if the Chef was born in or before
+     * 1780, and otherwise returns false.
+     */public boolean WellPracticed()
+    {
+        return(Integer.parseInt(this.Chef.getyearOfQualification()) < 1780);
+    }
+
+    /**
+     * Returns a string describing this object. 
+     */
+    public String toString()
+    {
+        return "style: " + this.style + ", Chef: " + this.Chef + ", Insured for: " + this.getRating() + ".";
+    }
+
+}
---
+++ M250/TMA_02/TMA02Download/TMA_Q2_Sol/chef.java
@@ -0,0 +1,93 @@
+/**
+ * Chef represents a chef such as Gordon Ramsey.
+ * To keep things simple we just have a name and a year of qualification.
+ * In the context of the Dish example, this is a component class.
+ * @author Tom Drew
+ * @version 1.0 21/01/2021
+ *
+ */
+public class Chef
+{
+    // instance variables
+    private String name; // a chef has-a name, e.g. "Gordon"
+    private String yearOfQualification; // a chef has-a year of qualifcation, e.g. "1985"
+
+
+    /**
+     * zero-argument constructor: if we don’t know the chef’s attributes.
+     */
+    public Chef()
+    {
+        this.name = null; //indicates the chef's name is unknown
+        this.yearOfQualification = null; //indicates year of qualification is unknown
+    }
+
+    /**
+     * A constructor for objects of class Chef if we know their name
+     * but not their year of qualification.
+     */
+    public Chef(String aName)
+    {
+        this.name = aName;
+        this.yearOfQualification = null; //indicates year of qualification is unknown
+    }
+
+    /**
+     * A constructor for objects of class Chef
+     * if we know their name and their year of qualification.
+     */
+    public Chef(String aName, String aYear)
+    {
+        this.name = aName;
+        this.yearOfQualification = aYear;
+    }
+
+    /**
+     * Setter for a chef's name.
+     */
+    public void setName(String aName)
+    {
+        this.name = aName;
+    }
+
+    /**
+     * Setter for a chef's year of qualification.
+     */
+    public void setYearOfQualification(String aYear)
+    {
+        this.yearOfQualification = aYear;
+    }
+
+    /**
+     * Getter for a chef's name. It could
+     * return a string such as "unattributed"
+     * if the chef's name is not known.
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * Getter for a chef's year of qualification.
+     * Similar to getName().
+     */
+    public String getYearOfQualification()
+    {
+        return this.yearOfQualification;
+    }
+
+    /**
+     * Returns a description of this object as a string.
+     */
+    public String toString()
+    {
+        return this.name + "(Qualified " + this.yearOfQualification + ")";
+    }
+
+    /**
+     * Composition Supplement Exercise 3(a)
+     * Modify the toString() method above so that it
+     * returns a useful message even if name and year of qualification are null.
+     */
+}
