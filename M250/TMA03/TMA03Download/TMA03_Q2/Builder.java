59fccd994ccdda3cc0d52fb6a16ca58d060f7307
Tom Drew <ihavenolife@hotmail.co.uk>
2021-05-02 01:37:48 +0100

    Q2 1aii

--- M250/TMA03/TMA03Download/TMA03_Q2/Builder.java
+++ M250/TMA03/TMA03Download/TMA03_Q2/Builder.java
@@ -8,13 +8,74 @@ import java.util.*;
  * @version  
  */
  public class Builder
- {
-   /**
+{
+    /**
      * Constructor for objects of class Builder
      */
-     public Builder()
-     {
+    public Builder()
+    {
         super();
-     }
-   
+        contractors = new HashMap<>();
+    }
+
+    private Map<String, Contractor> contractors;
+
+    public void addContractor(String aReg, String aName, String anAddress, String aTrade)
+    {
+        contractors.put(aReg, new Contractor(aName, anAddress, aTrade));
+    }
+
+    public void printContractor()
+    {
+        for (String aReg : contractors.keySet())
+        {
+            System.out.println(contractors);
+        }
+    }
+
+    public Contractor findContractor(String regFind)
+    {
+        Set<String> contractorsKeys = contractors.keySet(); for (String aReg: contractorsKeys)
+        {
+            System.out.println("registration " + regFind + " is " + contractors.get(regFind));
+            Contractor = regFind;
+            return Contractor;
+        }
+
+    }
+
+    //public String findContractorsForTrade(String trade)
+    //{
+        //Set<String> tradeSet = new HashSet<String>();
+
+        //contractors.put("plumber", getContractor);
+        //tradeSet = contractors.get(trade);
+        //System.out.println("the trade is "
+                //+ tradeSet);
+
+        //for (String aTrade : contractors.keySet())
+        //{
+        //    tradeSet = contractors.get(aTrade);
+          //  System.out.println(aTrade + " is interested in: "
+            //        + tradeSet);
+        //}
+    //}
+
+
+
 }
+   
+   
+
+
+
+
+
+    
+
+     
+
+
+
+
+     
