15c68007aa1995c1eb521b45629548bf0b88c2ad
Tom Drew <ihavenolife@hotmail.co.uk>
2021-05-03 01:54:37 +0100

    Q4 1ai

--- M250/TMA03/TMA03Download/TMA03_Q4/CyclingAdmin.java
+++ M250/TMA03/TMA03Download/TMA03_Q4/CyclingAdmin.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.io.*;
+import ou.*;
+
 /**
  * Class CyclingAdmin - carries out administrative tasks for a cycling race.
  * 
@@ -8,10 +12,16 @@ public class CyclingAdmin
 {
   
    /* instance variables */
+   /**
+    * Q4bi
+    */
+   private List<Cyclist> cyclists;
    
    /* static variables */
    public static final int MIN_RACE_TIME = 6000;   //the minimum time to
             //complete the race in tenths of a second (i.e. 10 mins)
+
+
    
    /**
     * Constructor for objects of class CyclingAdmin
@@ -19,7 +29,9 @@ public class CyclingAdmin
    public CyclingAdmin()
    {
       super();
+      cyclists = new arrayList<Cyclist>();
    }
+
  
 /* instance methods */
    
@@ -27,9 +39,9 @@ public class CyclingAdmin
    /**
     * Clears the cyclists list.
     */   
-//    public void clear()
-//    {
-//       this.cyclists.clear();
-//    }
+    public void clear()
+    {
+       this.cyclists.clear();
+    }
 }
 
