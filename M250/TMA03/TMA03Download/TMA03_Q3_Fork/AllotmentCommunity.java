ecec7e8805ecd5776faf8d1b2fb41146fbd316ed
Tom Drew <ihavenolife@hotmail.co.uk>
2021-05-06 00:43:51 +0100

    start

---
+++ M250/TMA03/TMA03Download/TMA03_Q3_Fork/AllotmentCommunity.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import ou.*;
+
+/**
+ * A class which simulates a simple dating agency using a map.
+ *
+ * @author M250 Module Team
+ * @version 1.0
+ */
+
+public class GardenTracker
+{
+    Map<String, Set<String>> bed = new HashMap<>();
+    Set<String> growing = new HashSet<>();
+
+    /**
+     * Stores and retrieves data about client names and interests.
+     */
+    //public void Tracker()
+    //{
+
+    //}
+
+
+        //phonebook.put("Captain Pugwash", "6001");
+
+
+
+
+
+}
